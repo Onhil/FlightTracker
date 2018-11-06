dd0ecb658afc92f508e0ae0fe6599004fc43ad75
sondrex76 <sonwrop@gmail.com>
2018-11-06 18:50:29 +0100

    Added empty tests

---
+++ database_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+
+}
+
+func TestAddMany(t *testing.T) {
+
+}
+
+func TestCount(t *testing.T) {
+
+}
+
+func TestGetICAO24(t *testing.T) {
+
+}
+
+func TestGetOriginCountry(t *testing.T) {
+
+}
