438cad255c37191aa3b1a8e44b98e13af1cfb114
sondrex76 <sonwrop@gmail.com>
2018-11-06 19:05:12 +0100

    Added teardown and setup to database test document

--- database_test.go
+++ database_test.go
@@ -1,8 +1,39 @@
 package main
 
 import (
+	"github.com/globalsign/mgo"
 	"testing"
 )
+func setupDB(t *testing.T) *Database {
+	db := Database{
+		"mongodb://localhost",
+		"testTrackDB",
+		"Tracks",
+	}
+
+	session, err := mgo.Dial(db.HostURL)
+	defer session.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	return &db
+}
+
+func tearDownDB(t *testing.T, db *Database) {
+	session, err := mgo.Dial(db.HostURL)
+	defer session.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = session.DB(db.DatabaseName).DropDatabase()
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestAdd(t *testing.T) {
 
