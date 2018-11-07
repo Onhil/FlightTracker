0c0c3c677645a500f15abf4ed7ced2c501110794
sondrex76 <sonwrop@gmail.com>
2018-11-07 14:03:44 +0100

    Added test to AddMany, it does not seem to correctly add elements as the count returns 0, not 3

--- database_test.go
+++ database_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/globalsign/mgo"
 	"testing"
 )
@@ -56,10 +57,40 @@ func TestAdd(t *testing.T) {
 }
 
 func TestAddMany(t *testing.T) {
+	db := setupDB(t)
+	defer tearDownDB(t, db)
 
-}
+	db.Init()
+	if db.Count() != 0 {
+		t.Error("Database not properly initialized, database count should be 0")
+	}
 
-func TestCount(t *testing.T) {
+	testState1 := State{"A", "D", "I", float64(18), float64(12), float64(400), false, float64(250), float64(25), float64(28), float64(31), "a", true}
+	testState2 := State{"B", "E", "H", float64(19), float64(13), float64(500), false, float64(251), float64(26), float64(29), float64(32), "b", true}
+	testState3 := State{"C", "F", "G", float64(20), float64(14), float64(600), false, float64(252), float64(27), float64(30), float64(33), "c", true}
+
+	d := []State{}
+	d = append(d, testState1)
+	d = append(d, testState2)
+	d = append(d, testState3)
+
+	db.AddMany(d)
+
+
+	//err := db.Add(testState1)
+	//err = db.Add(testState2)
+	//err = db.Add(testState3)
+
+	//if err != nil {
+	//	t.Error(err)
+	//}
+
+
+	if db.Count() != 3 {
+		fmt.Print(db.Count()) // DEBUG
+		fmt.Print("\n") // DEBUG
+		t.Error("Database not properly initialized, database count should be 3")
+	}
 
 }
 
