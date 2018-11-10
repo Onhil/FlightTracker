be848b19c388e689832e04d0bac5585c092ca775
Bjornaahr <bjornar.hammerseth@gmail.com>
2018-11-10 15:05:42 +0100

    Put the url in the for loop :derp:

--- openstack.go
+++ openstack.go
@@ -10,31 +10,31 @@ import (
 
 // Run _
 func Run() {
-	// Should probably be inside for loop?
-	resp, err := http.Get("https://opensky-network.org/api/states/all")
-	if err != nil {
+
+	for {
+		resp, err := http.Get("https://opensky-network.org/api/states/all")
 		if err != nil {
 			fmt.Println(err)
 		}
-	} else {
-		for {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-			var state States
-			if err = json.Unmarshal(body, &state); err != nil {
-				fmt.Println(err)
-			}
-			var sarray []interface{}
-			for i := range state.States {
-				sarray = append(sarray, state.States[i])
-			}
-			err = DBValues.Add(sarray, DBValues.CollectionName)
-			if err != nil {
-				fmt.Println(err)
-			}
-			time.Sleep(15 * time.Minute)
+
+		fmt.Println("Enter loop")
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
 		}
+		var state States
+		if err = json.Unmarshal(body, &state); err != nil {
+			fmt.Println(err)
+		}
+		var sarray []interface{}
+		for i := range state.States {
+			sarray = append(sarray, state.States[i])
+		}
+		err = DBValues.Add(sarray, DBValues.CollectionName)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(30 * time.Second)
+		fmt.Println("Next update in 15 min")
 	}
 }
