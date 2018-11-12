46a445cf77585d89550de0ad46e0d007e51600fa
sondrex76 <sonwrop@gmail.com>
2018-11-12 11:32:17 +0100

    Began working on TestOriginCountryHandler

--- main_test.go
+++ main_test.go
@@ -20,11 +20,40 @@ func TestPlaneHandler(t *testing.T) {
 }
 
 func TestOriginCountryHandler(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(PlaneHandler))
+	// Starts the database
+	db := setupDB(t)
+	defer tearDownDB(t, db)
+
+	db.Init()
+	if db.Count(db.CollectionState) != 0 {
+		t.Error("Database not properly initialized, database count should be 0")
+	}
+
+
+	// adds state with country as one of its values
+	testState := State{"A", "B", "C", float64(18), float64(12), float64(400), false, float64(250), float64(19), float64(18), float64(16), "", true}
+	var testStateArray []interface{}
+	testStateArray = append(testStateArray, testState)
+
+	err := db.Add(testStateArray, db.CollectionState)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if db.Count(db.CollectionState) != 1 {
+		t.Error("Database not properly initialized, database count should be 1")
+	}
+
+
+	// Actual test of the handler in question
+	ts := httptest.NewServer(http.HandlerFunc(OriginCountryHandler))
 	defer ts.Close()
+	// WIP
+	// _, err = http.Get(ts.URL + "/{country:C}")
 
-	// resp, err := http.Get(ts.URL + "/country/{country:[A-Za-z_ ]+}")
-
+	if err != nil {
+		// t.Error(err)
+	}
 }
 
 func TestDepartureHandler(t *testing.T) {
