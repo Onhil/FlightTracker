366357263151d77046b142205f8a0f27a71b5f16
sondrex76 <sonwrop@gmail.com>
2018-11-12 09:01:49 +0100

    Wrote a new add test for the modifications made in the code

--- database_test.go
+++ database_test.go
@@ -38,103 +38,26 @@ func tearDownDB(t *testing.T, db *Database) {
 	}
 }
 
-/// Commented out likely not needed as UpdateState is changed to removing all documents and adding new ones therefore no duplicates
-
-/* func TestAddDuplicates(t *testing.T) {
+func TestAdd(t *testing.T) {
 	db := setupDB(t)
 	defer tearDownDB(t, db)
 
 	db.Init()
-	if db.Count() != 0 {
+	if db.Count(db.CollectionState) != 0 {
 		t.Error("Database not properly initialized, database count should be 0")
 	}
 
+	var sarray []interface{}
 	testState := State{"A", "B", "C", float64(18), float64(12), float64(400), false, float64(250), float64(19), float64(18), float64(16), "", true}
-	err := db.Add(testState)
+	sarray = append(sarray, testState)
+
+	err := db.Add(sarray, "testState")
+	
 	if err != nil {
 		t.Error(err)
 	}
-	if db.Count() != 1 {
+	if db.Count(db.CollectionState) != 1 {
 		t.Error("Database not properly initialized, database count should be 1")
-	}
-
-	err = db.Add(testState)
-	if err == nil {
-		t.Error("An error should have been returned when trying to insert a duplicate element!")
-	}
-	if db.Count() != 1 {
-		t.Error("Duplicate got added, database count should be 1")
-	}
-} */
-
-/* func TestAddManyDuplicates(t *testing.T) {
-	db := setupDB(t)
-	defer tearDownDB(t, db)
-
-	db.Init()
-	if db.Count() != 0 {
-		t.Error("Database not properly initialized, database count should be 0")
-	}
-
-	testState1 := State{"A", "D", "I", float64(18), float64(12), float64(400), false, float64(250), float64(25), float64(28), float64(31), "a", true}
-	testState2 := State{"B", "E", "H", float64(19), float64(13), float64(500), false, float64(251), float64(26), float64(29), float64(32), "b", true}
-	testState3 := State{"C", "F", "G", float64(20), float64(14), float64(600), false, float64(252), float64(27), float64(30), float64(33), "c", true}
-
-	d := []State{}
-	d = append(d, testState1)
-	d = append(d, testState2)
-	d = append(d, testState3)
-
-	db.AddMany(d)
-
-	if db.Count() != 3 {
-		fmt.Print(db.Count()) // DEBUG
-		fmt.Print("\n")       // DEBUG
-		t.Error("Database not properly initialized, database count should be 3")
-	}
-
-	err := db.AddMany(d)
-
-	if err == nil {
-		t.Error("An error should have been returned when trying to insert duplicate elements!")
-	}
-
-	if db.Count() != 3 {
-		fmt.Print(db.Count()) // DEBUG
-		fmt.Print("\n")       // DEBUG
-		t.Error("Duplicates added, database count should be 3")
-	}
-
-} */
-
-func TestAddAirport(t *testing.T) {
-	db := setupDB(t)
-	defer tearDownDB(t, db)
-
-	db.Init()
-	if db.Count(db.CollectionAirport) != 0 {
-		t.Error("Database not properly initialized, database count should be 0")
-	}
-
-	testAirport1 := Airport{1, "Gjovik Airport", "Gjovik", "Norway", "GJO", "GJOV", float64(10), float64(24), float64(500), float64(100), "E", "Norway/Oslo", "airport", "test"}
-	testAirport2 := Airport{2, "Bardufoss Airport", "Bardufoss", "Norway", "BAR", "BARD", float64(10), float64(24), float64(500), float64(100), "E", "Norway/Oslo", "airport", "test"}
-	testAirport3 := Airport{3, "Molvik Airport", "Molvik", "Norway", "MOL", "MOLV", float64(10), float64(24), float64(500), float64(100), "E", "Norway/Oslo", "airport", "test"}
-
-	var d []interface{}
-	d = append(d, testAirport1)
-	d = append(d, testAirport2)
-	d = append(d, testAirport3)
-
-	err := db.Add(d, db.CollectionAirport)
-
-	if err != nil {
-		t.Error("There should not have been any errors!")
-	}
-
-	if db.Count(db.CollectionAirport) != 3 {
-		fmt.Print(db.Count(db.CollectionAirport)) // DEBUG
-		fmt.Print("\n")                           // DEBUG
-		t.Error("Database not properly initialized, database count should be 3")
 	}
 }
 
@@ -232,7 +155,113 @@ func TestGetAirport(t *testing.T) {
 	}
 }
 
+func TestAddAirport(t *testing.T) {
+	db := setupDB(t)
+	defer tearDownDB(t, db)
+
+	db.Init()
+	if db.Count(db.CollectionAirport) != 0 {
+		t.Error("Database not properly initialized, database count should be 0")
+	}
+
+	testAirport1 := Airport{1, "Gjovik Airport", "Gjovik", "Norway", "GJO", "GJOV", float64(10), float64(24), float64(500), float64(100), "E", "Norway/Oslo", "airport", "test"}
+	testAirport2 := Airport{2, "Bardufoss Airport", "Bardufoss", "Norway", "BAR", "BARD", float64(10), float64(24), float64(500), float64(100), "E", "Norway/Oslo", "airport", "test"}
+	testAirport3 := Airport{3, "Molvik Airport", "Molvik", "Norway", "MOL", "MOLV", float64(10), float64(24), float64(500), float64(100), "E", "Norway/Oslo", "airport", "test"}
+
+	var d []interface{}
+	d = append(d, testAirport1)
+	d = append(d, testAirport2)
+	d = append(d, testAirport3)
+
+	err := db.Add(d, db.CollectionAirport)
+
+	if err != nil {
+		t.Error("There should not have been any errors!")
+	}
+
+	if db.Count(db.CollectionAirport) != 3 {
+		fmt.Print(db.Count(db.CollectionAirport)) // DEBUG
+		fmt.Print("\n")                           // DEBUG
+		t.Error("Database not properly initialized, database count should be 3")
+	}
+}
+
+func TestGetAllFlights(t *testing.T) {
+
+}
+
+func TestGetFlightFieldData(t *testing.T) {
+
+}
+
 /// Commented out likely not needed as UpdateState is changed to removing all documents and adding new ones
+
+/* func TestAddDuplicates(t *testing.T) {
+	db := setupDB(t)
+	defer tearDownDB(t, db)
+
+	db.Init()
+	if db.Count() != 0 {
+		t.Error("Database not properly initialized, database count should be 0")
+	}
+
+	testState := State{"A", "B", "C", float64(18), float64(12), float64(400), false, float64(250), float64(19), float64(18), float64(16), "", true}
+	err := db.Add(testState)
+	if err != nil {
+		t.Error(err)
+	}
+	if db.Count() != 1 {
+		t.Error("Database not properly initialized, database count should be 1")
+	}
+
+	err = db.Add(testState)
+	if err == nil {
+		t.Error("An error should have been returned when trying to insert a duplicate element!")
+	}
+	if db.Count() != 1 {
+		t.Error("Duplicate got added, database count should be 1")
+	}
+} */
+
+/* func TestAddManyDuplicates(t *testing.T) {
+	db := setupDB(t)
+	defer tearDownDB(t, db)
+
+	db.Init()
+	if db.Count() != 0 {
+		t.Error("Database not properly initialized, database count should be 0")
+	}
+
+	testState1 := State{"A", "D", "I", float64(18), float64(12), float64(400), false, float64(250), float64(25), float64(28), float64(31), "a", true}
+	testState2 := State{"B", "E", "H", float64(19), float64(13), float64(500), false, float64(251), float64(26), float64(29), float64(32), "b", true}
+	testState3 := State{"C", "F", "G", float64(20), float64(14), float64(600), false, float64(252), float64(27), float64(30), float64(33), "c", true}
+
+	d := []State{}
+	d = append(d, testState1)
+	d = append(d, testState2)
+	d = append(d, testState3)
+
+	db.AddMany(d)
+
+	if db.Count() != 3 {
+		fmt.Print(db.Count()) // DEBUG
+		fmt.Print("\n")       // DEBUG
+		t.Error("Database not properly initialized, database count should be 3")
+	}
+
+	err := db.AddMany(d)
+
+	if err == nil {
+		t.Error("An error should have been returned when trying to insert duplicate elements!")
+	}
+
+	if db.Count() != 3 {
+		fmt.Print(db.Count()) // DEBUG
+		fmt.Print("\n")       // DEBUG
+		t.Error("Duplicates added, database count should be 3")
+	}
+
+} */
 
 /* func TestUpdateState(t *testing.T) {
 	db := setupDB(t)
