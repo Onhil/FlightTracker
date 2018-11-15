3b870399c2341fc79721e140030680b2e764a6ba
sondrex76 <sonwrop@gmail.com>
2018-11-15 17:10:20 +0100

    Added airports to the test, still dosen't return the correct value

--- maphandler_test.go
+++ maphandler_test.go
@@ -81,6 +81,9 @@ func TestCountryMapHandler(t *testing.T) { // The function to be tested is not y
 	defer tearDownDB(t, DBValues)
 
 	DBValues.Init()
+	if DBValues.Count(DBValues.CollectionState) != 0 {
+		t.Error("Database not properly initialized, database count should be 0")
+	}
 
 	var sarray []interface{}
 	testState := State{"A", "B", "C", float64(18), float64(12), float64(400), false, float64(250), float64(19), float64(18), float64(16), "", true}
@@ -93,10 +96,13 @@ func TestCountryMapHandler(t *testing.T) { // The function to be tested is not y
 	if err != nil {
 		t.Error(err)
 	}
+	if DBValues.Count(DBValues.CollectionState) != 2 {
+		t.Error("Database not properly initialized, database count should be 1")
+	}
 
 	var testFlightArray []interface{}
 	testFlight := Flight{"A", 1, "B", 1, "Reku", "C"}
-	testFlight1 := Flight{"D", 1, "E", 1, "Rek", "F"}
+	testFlight1 := Flight{"D", 1, "E", 1, "Reku", "F"}
 	testFlight2 := Flight{"G", 1, "H", 1, "Reku", "I"}
 	testFlightArray = append(testFlightArray, testFlight)
 	testFlightArray = append(testFlightArray, testFlight1)
@@ -106,6 +112,24 @@ func TestCountryMapHandler(t *testing.T) { // The function to be tested is not y
 
 	if err != nil {
 		t.Error(err)
+	}
+	if DBValues.Count(DBValues.CollectionFlight) != 3 {
+		t.Error("Database not properly initialized, database count should be 1")
+	}
+
+	testAirport1 := Airport{1, "Reku", "Gjovik", "Mekka", "GJO", "GJOV", float64(10), float64(24), float64(500), "100", "E", "Norway/Oslo", "airport", "test"}
+	testAirport2 := Airport{2, "A", "Bardufoss", "Norway", "BAR", "BARD", float64(10), float64(24), float64(500), "100", "E", "Norway/Oslo", "airport", "test"}
+	testAirport3 := Airport{3, "Molvik Airport", "Molvik", "Norway", "MOL", "MOLV", float64(10), float64(24), float64(500), "100", "E", "Norway/Oslo", "airport", "test"}
+
+	var d []interface{}
+	d = append(d, testAirport1)
+	d = append(d, testAirport2)
+	d = append(d, testAirport3)
+
+	err = DBValues.Add(d, DBValues.CollectionAirport)
+
+	if err != nil {
+		t.Error("There should not have been any errors!")
 	}
 
 	ts := httptest.NewServer(http.HandlerFunc(PlaneMapHandler))
